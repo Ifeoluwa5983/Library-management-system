d2100914852167495e64ad6a0fcb7b974138c1a8
Ife5983 <modulehelen44@gmail.com>
2020-11-13 16:46:19 -0800

    Changes to be committed:

---
+++ src/main/java/com/librarryMS/library/controller/LibraryController.java
@@ -0,0 +1,56 @@
+package com.librarryMS.library.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.librarryMS.library.entity.Book;
+import com.librarryMS.library.service.LibraryServiceImpl;
+
+@Controller
+public class LibraryController {
+	
+	@Autowired
+	private LibraryServiceImpl lsi;
+	
+	@GetMapping("/library")
+	public List<Book> getBooks(){
+		return lsi.findAll();
+	}
+
+	@GetMapping("/library/{title}")
+	public ResponseEntity<Book> findById(@PathVariable String title) {
+		Optional<Book> oneBook =  lsi.getBookByName(title);
+		if (!oneBook.isPresent()) {
+			return null;
+		}
+		return ResponseEntity.ok(oneBook.get());
+	}
+	
+	@DeleteMapping("/book/{title}")
+	public void deleteByName(@PathVariable String title) {
+		lsi.deleteBookByName(title);
+	}
+	
+	@PatchMapping("/library/book/{title}")
+	public void updateBook(@RequestBody Book book) {
+		book.getId();
+		lsi.addBook(book);
+	}
+	
+	@PostMapping("/addBook")
+	public Book addBook(@RequestBody Book book) {
+		book.setId(0);
+		lsi.addBook(book);
+		return book;
+	}
+}
